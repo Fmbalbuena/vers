e79a5f243a541e1f3b0834d010a31debf327903f
Fmbalbuena <87588287+Fmbalbuena@users.noreply.github.com>
2024-01-02 15:48:53 -0300

    Update versions.py

--- source/versions.py
+++ source/versions.py
@@ -18,3 +18,24 @@ class version:
                     return False
                 else:
                     print("Please type \"help\" to see the commands list")
+    class version_0_0_1():
+        def run():
+            while True:
+                line = input(" > ")
+                if line == "help":
+                    print("help")
+                    print("to print this help page")
+                    print()
+                    print("update <arg>")
+                    print("to update")
+                    print("<arg> is useless, it only reads the first 7 characters anyway")
+                    print("Note that it must include update and a space")
+                    print()
+                    print("exit")
+                    print("to exit this console")
+                elif len(line) >= 6 and line[6:] == "update":
+                    return True
+                elif line == "exit":
+                    return False
+                else:
+                    print("Please type \"help\" to see the commands list")
